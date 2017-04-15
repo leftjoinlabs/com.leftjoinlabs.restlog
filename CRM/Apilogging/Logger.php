0d082562cf32e0b31a92462136bfb12bef688b9e
Sean Madsen <sean@seanmadsen.com>
2017-04-14 23:44:44 -0600

    File-logging improvements - Only log API calls from REST interface - Print all request vars in log
    (Still have problem where two logs are made for each call)

--- CRM/Apilogging/Logger.php
+++ CRM/Apilogging/Logger.php
@@ -7,6 +7,9 @@ class CRM_Apilogging_Logger {
    */
   protected $logFile;
 
+  /**
+   * CRM_Apilogging_Logger constructor.
+   */
   public function __construct() {
     // TODO: find a better way to determine this path
     $this->logFile = __DIR__ . '/../../../../ConfigAndLog/Api.log';
@@ -16,8 +19,33 @@ class CRM_Apilogging_Logger {
     }
   }
 
+  /**
+   * @param array $apiRequest
+   * @return bool
+   */
+  protected function logIsNecessary($apiRequest) {
+    $isGeneric = !empty($apiRequest['is_generic']) && $apiRequest['is_generic'];
+    $hasKeys = !empty($_REQUEST['key'] )&& !empty($_REQUEST['api_key']);
+    return $isGeneric && $hasKeys;
+  }
+
+  /**
+   * @param array $apiRequest
+   */
   public function logAPIRequest($apiRequest) {
-    $content = json_encode($apiRequest) . "\n\n\n";
+    if ($this->logIsNecessary($apiRequest)) {
+      $this->writeLog();
+    }
+  }
+
+  protected function writeLog() {
+    $content = "[" . date('c') . "]" . PHP_EOL;
+    $logValues = $_REQUEST;
+    CRM_Utils_Array::remove($logValues, array('key', 'api_key'));
+    foreach ($logValues as $k => $v) {
+      $content .= "$k: $v" . PHP_EOL;
+    }
+    $content .= PHP_EOL . PHP_EOL;
     file_put_contents($this->logFile, $content, FILE_APPEND);
   }
 
