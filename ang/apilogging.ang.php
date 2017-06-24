58c7689698ff53d1affffe80f4682b3c29eeffae
Sean Madsen <sean@seanmadsen.com>
2017-06-23 18:31:30 -0600

    civix generate:angular-module --am=apilogging

---
+++ ang/apilogging.ang.php
@@ -0,0 +1,24 @@
+<?php
+// This file declares an Angular module which can be autoloaded
+// in CiviCRM. See also:
+// http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_angularModules
+
+return array (
+  'js' => 
+  array (
+    0 => 'ang/apilogging.js',
+    1 => 'ang/apilogging/*.js',
+    2 => 'ang/apilogging/*/*.js',
+  ),
+  'css' => 
+  array (
+    0 => 'ang/apilogging.css',
+  ),
+  'partials' => 
+  array (
+    0 => 'ang/apilogging',
+  ),
+  'settings' => 
+  array (
+  ),
+);
