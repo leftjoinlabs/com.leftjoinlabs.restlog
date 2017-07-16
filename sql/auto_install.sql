d4d58d809852d707dced07174dc5e6a21b1108b7
Sean Madsen <sean@seanmadsen.com>
2017-07-16 13:09:56 -0600

    Add indexes to table

--- sql/auto_install.sql
+++ sql/auto_install.sql
@@ -8,15 +8,24 @@ DROP TABLE IF EXISTS `civicrm_apilogginglog`;
 -- *
 -- *******************************************************/
 CREATE TABLE `civicrm_apilogginglog` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT  COMMENT 'Unique ApiloggingLog ID',
-  `time_stamp` datetime NOT NULL,
-  `calling_contact_id` int unsigned  COMMENT 'FK to the id of the contact who made the API call',
-  `entity` char(100),
-  `action` char(100),
-  `parameters` text,
+  `id`                 INT UNSIGNED NOT NULL AUTO_INCREMENT
+  COMMENT 'Unique ApiloggingLog ID',
+  `time_stamp`         DATETIME     NOT NULL,
+  `calling_contact_id` INT UNSIGNED
+  COMMENT 'FK to the id of the contact who made the API call',
+  `entity`             CHAR(100),
+  `action`             CHAR(100),
+  `parameters`         TEXT,
   PRIMARY KEY (`id`),
+  INDEX `index_time_stamp`(time_stamp),
+  INDEX `index_calling_contact_id`(calling_contact_id),
+  INDEX `index_entity`(entity),
+  INDEX `index_action`(action),
   CONSTRAINT FK_civicrm_apilogginglog_calling_contact_id
-    FOREIGN KEY (`calling_contact_id`)
-    REFERENCES `civicrm_contact`(`id`)
+  FOREIGN KEY (`calling_contact_id`)
+  REFERENCES `civicrm_contact` (`id`)
     ON DELETE SET NULL
-)  ENGINE=InnoDB DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+)
+  ENGINE = InnoDB
+  DEFAULT CHARACTER SET utf8
+  COLLATE utf8_unicode_ci;
